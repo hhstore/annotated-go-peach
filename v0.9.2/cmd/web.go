4a0ccdc0cd28864b7d44ad4bb29be6d6921094c2
hhstore <hhstore@qq.com>
2016-10-31 15:43:07 +0800

    update - 注解关键路由分支.

--- v0.9.2/cmd/web.go
+++ v0.9.2/cmd/web.go
@@ -94,13 +94,13 @@ func runWeb(ctx *cli.Context) {
 	//---------------------------------------
 	//          路由配置:
 	//---------------------------------------
-	m.Get("/", routers.Home)		// 首页部分
+	m.Get("/", routers.Home)		// 首页部分: [github.com/peachdocs/peach/routers/home.go:30]
 	m.Get("/docs", routers.Docs)
 	m.Get("/docs/images/*", routers.DocsStatic)
 	m.Get("/docs/*", routers.Protect, routers.Docs)
 	m.Post("/hook", routers.Hook)
 	m.Get("/search", routers.Search)
-	m.Get("/*", routers.Pages)
+	m.Get("/*", routers.Pages)		// [页面遍历搜索, 找到即渲染页面]github.com/peachdocs/peach/routers/home.go:39
 
 	m.NotFound(routers.NotFound)
 
