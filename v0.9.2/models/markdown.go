7e7d76fb66f057c33ff519751001a49147744d3e
hhstore <hhstore@qq.com>
2016-10-31 15:47:31 +0800

    update - 模块功能: markdown 文件解析+渲染, 注解完成.

--- v0.9.2/models/markdown.go
+++ v0.9.2/models/markdown.go
@@ -29,12 +29,15 @@ type MarkdownRender struct {
 	blackfriday.Renderer
 }
 
+// tab 符号转换成4空格
 func (mr *MarkdownRender) BlockCode(out *bytes.Buffer, text []byte, lang string) {
 	var tmp bytes.Buffer
 	mr.Renderer.BlockCode(&tmp, text, lang)
-	out.Write(bytes.Replace(tmp.Bytes(), tab, spaces, -1))
+	out.Write(bytes.Replace(tmp.Bytes(), tab, spaces, -1))	// 把 tab 替换成4空格, 然后写出到 out 里.
 }
 
+// markdown 处理:
+//	- 解析并渲染 markdown
 func markdown(raw []byte) []byte {
 	htmlFlags := 0 |
 		blackfriday.HTML_USE_XHTML |
@@ -55,5 +58,5 @@ func markdown(raw []byte) []byte {
 		blackfriday.EXTENSION_SPACE_HEADERS |
 		blackfriday.EXTENSION_HEADER_IDS
 
-	return blackfriday.Markdown(raw, renderer, extensions)
+	return blackfriday.Markdown(raw, renderer, extensions)	// 调用第三方包, 解析并渲染 markdown.
 }
