3df32720ce501220f9608c4ca74514318ce6c2dd
hhstore <hhstore@qq.com>
2016-10-31 15:55:20 +0800

    update - 关键路由: docs 部分.

--- v0.9.2/cmd/web.go
+++ v0.9.2/cmd/web.go
@@ -94,13 +94,13 @@ func runWeb(ctx *cli.Context) {
 	//---------------------------------------
 	//          路由配置:
 	//---------------------------------------
-	m.Get("/", routers.Home)		// 首页部分: [github.com/peachdocs/peach/routers/home.go:30]
-	m.Get("/docs", routers.Docs)
+	m.Get("/", routers.Home)		// 首页部分路由: [github.com/peachdocs/peach/routers/home.go:30]
+	m.Get("/docs", routers.Docs)	// 文档部分路由: [github.com/peachdocs/peach/routers/docs.go:44]
 	m.Get("/docs/images/*", routers.DocsStatic)
 	m.Get("/docs/*", routers.Protect, routers.Docs)
 	m.Post("/hook", routers.Hook)
-	m.Get("/search", routers.Search)	// 搜索页面路由
-	m.Get("/*", routers.Pages)		// [页面遍历搜索, 找到即渲染页面]github.com/peachdocs/peach/routers/home.go:39
+	m.Get("/search", routers.Search)	// 搜索页面路由: [github.com/peachdocs/peach/routers/search.go:24]
+	m.Get("/*", routers.Pages)			// [页面遍历搜索, 找到即渲染页面]github.com/peachdocs/peach/routers/home.go:39
 
 	m.NotFound(routers.NotFound)
 
